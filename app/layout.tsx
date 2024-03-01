a1a760e09215e7ed885173e8e4397e6cbcc8f543
Dat Phan <datdoof@gmail.com>
2024-02-29 23:52:15 -0600

    commiting

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Spotify Clone",
+  description: "Listen to music from Spotify",
+};
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const userSongs = await getSongsByUserId();
+  const products = await getActiveProductsWithPrices();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player/>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
